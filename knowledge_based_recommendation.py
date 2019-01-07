a394b61313b04ae32fb6965b50c1195996fe666c
Karvendhan <karvsmech@gmail.com>
2019-01-07 20:42:13 +0530

    Feature Addition: Knowledge_Based_Recommendations

--- knowledge_based_recommendation.py
+++ knowledge_based_recommendation.py
@@ -11,6 +11,15 @@ del movies['Unnamed: 0']
 del reviews['Unnamed: 0']
 
 # Find the most popular movies:
+# The task is no matter the user, we need to provide a list of the
+# recommendations based on simply the most popular items.
+
+For this task, we will consider what is "most popular" based on the following criteria:
+
+A movie with the highest average rating is considered best
+With ties, movies that have more ratings are better
+A movie must have a minimum of 5 ratings to be considered among the best movies
+If movies are tied in their average rating and number of ratings, the ranking is determined by the movie that is the most recent rating
 
 
 
